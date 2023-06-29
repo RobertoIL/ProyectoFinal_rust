8ef1438f34d70f2d57c7914072850c99fdd060d6
Roberto <robertillesca1@gmail.com>
2023-06-29 10:28:37 -0400

    .........

--- src/main.rs
+++ src/main.rs
@@ -36,8 +36,13 @@ impl ListaDeTareas{
         self.tareas.push(tarea);
     }
 
-    fn completar_tarea(){
-
+    fn completar_tarea(&mut self, id_tarea: u32) {
+        if let Some(tarea) = self.tareas.iter_mut().find(|t| t.id == id_tarea) {
+            tarea.completeda = true;
+            Ok(())
+        } else {
+            Err("Tarea no encontrada".to_string())
+        }
     }
 }
 
