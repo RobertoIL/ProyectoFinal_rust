5158bfc6c14da5b07278272db7eb5f4776ac3892
Roberto <robertillesca1@gmail.com>
2023-06-29 17:03:03 -0400

    ....

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use chrono::{DateTime, Utc};
+use std::fs::OpenOptions;
+use std::io::{BufRead, BufReader, Write};
 
 struct Usuario{
     nombre: String,
@@ -16,12 +18,14 @@ struct Tarea{
 struct ListaDeTareas{
     usuario: Usuario,
     tareas: Vec<Tarea>,
+    file_path: String,
 }
 impl ListaDeTareas{
     fn new(usuario: Usuario) -> ListaDeTareas {
         ListaDeTareas {
             usuario,
             tareas: Vec::new(),
+            file_path,
         }
     }
 
@@ -29,21 +33,40 @@ impl ListaDeTareas{
     fn agregar_tarea(&mut self, descripcion: String, fecha_hora: DateTime<Utc>){
         let id = self.tareas.len() as u32 + 1;
         let tarea = Tarea {
-            descripcion,
-            fecha_hora,
+            id,
+            descripcion: descripcion.clone(),
             completada: false,
+            fecha_hora,
         };
         self.tareas.push(tarea);
+        
     }
 
     fn completar_tarea(&mut self, id_tarea: u32) {
         if let Some(tarea) = self.tareas.iter_mut().find(|t| t.id == id_tarea) {
-            tarea.completeda = true;
-            Ok(())
+            tarea.completada = true;
+            Ok(());
         } else {
-            Err("Tarea no encontrada".to_string())
+            Err("Tarea no encontrada".to_string());
         }
     }
+
+    fn imprimir_tareas(&self){
+        println!("");
+        for tarea in &self.tareas {
+            let status = if tarea.completada {"[X]"} else {"[ ]"};
+            println!(
+                "{} Tarea {}: {} - Fecha y hora: {}",
+                status, tarea.id, tarea.descripcion, tarea.fecha_hora
+            );
+        }
+    }
+
+    fn agregar_tarea_txt(){
+        
+    }
+
+    
 }
 
 
