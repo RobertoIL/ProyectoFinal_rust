bbd73ae69b0e57e95d92250bec24660b59af85c6
Roberto <robertillesca1@gmail.com>
2023-06-29 23:42:22 -0400

    ....

--- src/main.rs
+++ src/main.rs
@@ -39,12 +39,14 @@ impl ListaDeTareas{
             fecha_hora,
         };
         self.tareas.push(tarea);
+        self.guardar_tarea_txt();
         
     }
 
     fn completar_tarea(&mut self, id_tarea: u32) {
         if let Some(tarea) = self.tareas.iter_mut().find(|t| t.id == id_tarea) {
             tarea.completada = true;
+            self.guardar_tarea_txt();
             Ok(());
         } else {
             Err("Tarea no encontrada".to_string());
@@ -62,8 +64,26 @@ impl ListaDeTareas{
         }
     }
 
-    fn agregar_tarea_txt(){
-        
+    fn guardar_tarea_txt(&self){
+        let mut file: OpenOptions::new()
+            .write(true)
+            .truncate(true)
+            .create(true)
+            .open(&self.file_path)
+            .unwrap();
+
+        for tarea in &self.tareas{
+            let tarea_str = format!(
+                "{};{};{};{}\n",
+                tarea.id,
+                tarea.descripcion,
+                tarea.fecha_hora.to_rfc3339(),
+                tarea.completada
+            );
+            file.write_all(tarea_str.as_bytes()).unwrap();
+
+        }
+
     }
 
     
